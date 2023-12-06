83468429b529694b2f1e6d1738c765635de57213
jiezhoujif <jiezhoujif@126.com>
2023-12-06 19:31:20 +0800

    some code change

--- evolutionary_game/evolution_of_environmental factors.py
+++ evolutionary_game/evolution_of_environmental factors.py
@@ -13,7 +13,7 @@ def Cooperation_proportion_derivatives(x, t, punish, b, xi):
     function_2 = xi * x[1] * (1 - x[1]) * (b*(1-x[0])+punish*(x[1]+x[0]-1)-x[0]*x[0]*(1+x[1]*punish))
     return [function_1, function_2]
 
-def get_plot(x,t):
+def get_plot(x,t,remark):
 
     Collaborator_ratio= [sublist[0] for sublist in x]
 
@@ -33,19 +33,26 @@ def get_plot(x,t):
     plt.ylabel('p')
     plt.title("Perceived_probability")
 
-    plt.suptitle("Copy equation simulation results")
+    plt.suptitle("Parameter settings {}".format(remark))
+
+    plt.savefig(r'C:/Users/NewNLPer/Desktop/za/exp_figure/{}.png'.format(remark))
     plt.show()
 
+
+def get_remark(b,punish,xi):
+    remark="b={}_punish={}_xi={}".format(b,punish,xi)
+    return remark
 
 
 if __name__=="__main__":
 
     initial_x=[0.5, 0.5] ##系统初始条件
     t = np.linspace(0, 800, 800)  #时间戳
-    punish = 0.1
-    b = 1
+    punish = 0.5
+    b = 2
     xi = 0.01
+    remark=get_remark(b,punish,xi)
     result = odeint(Cooperation_proportion_derivatives, initial_x, t, args=(punish, b, xi))
-    get_plot(result,t)
+    get_plot(result,t,remark)
 
 
