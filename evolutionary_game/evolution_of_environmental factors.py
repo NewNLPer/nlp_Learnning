a8ce655754b7fbb7cc51ce76a1ae4a2efe806de4
jiezhoujif <jiezhoujif@126.com>
2023-12-07 14:07:49 +0800

    some code change

--- evolutionary_game/evolution_of_environmental factors.py
+++ evolutionary_game/evolution_of_environmental factors.py
@@ -9,30 +9,52 @@ import numpy as np
 from scipy.integrate import odeint
 
 def Cooperation_proportion_derivatives(x, t, punish, b, xi):
-    function_1 = x[0] * (1 - x[0]) * (x[0]-b+punish+x[1]*punish*(x[0]-2))
-    function_2 = xi * x[1] * (1 - x[1]) * (b*(1-x[0])+punish*(x[1]+x[0]-1)-x[0]*x[0]*(1+x[1]*punish))
+    """
+    :param x:  Initial variable[x,ac,ad]
+    :param t: time
+    :param punish:
+    :param b: b=[1,2]
+    :param xi:Growth rate control
+    :return:
+    """
+    piC_piD=punish*x[0]*(x[1]+x[2])+x[0]-x[1]*punish-b*x[0]
 
-    return [function_1, function_2]
+    function_1 = x[0] * ( 1 - x[0] ) * piC_piD
+
+    function_2 = xi * x[1] * ( 1 - x[1] ) * -1 * piC_piD
+
+    function_3 = xi * x[2] * ( 1 - x[2] ) * piC_piD
+
+    return [function_1, function_2, function_3]
 
 def get_plot(x,t,remark):
 
     Collaborator_ratio= [sublist[0] for sublist in x]
 
-    Perceived_probability=[sublist[1] for sublist in x]
+    C_Perceived_degree=[sublist[1] for sublist in x]
+
+    D_Perceived_degree = [sublist[2] for sublist in x]
+
 
     plt.figure(figsize=(10, 10))
 
-    plt.subplot(2, 1, 1)
+    plt.subplot(3, 1, 1)
     plt.plot(t,Collaborator_ratio)
     plt.xlabel('t')
     plt.ylabel('c')
     plt.title("Collaborator_ratio")
 
-    plt.subplot(2, 1, 2)
-    plt.plot(t,Perceived_probability)
+    plt.subplot(3, 1, 2)
+    plt.plot(t,C_Perceived_degree)
     plt.xlabel('t')
     plt.ylabel('p')
-    plt.title("Perceived_probability")
+    plt.title("C_Perceived_degree")
+
+    plt.subplot(3, 1, 3)
+    plt.plot(t,D_Perceived_degree)
+    plt.xlabel('t')
+    plt.ylabel('p')
+    plt.title("D_Perceived_degree")
 
     plt.suptitle("Parameter settings {}".format(remark))
 
@@ -40,16 +62,18 @@ def get_plot(x,t,remark):
     plt.show()
 
 def get_remark(b,punish,xi):
+
     remark="b={}_punish={}_xi={}".format(b,punish,xi)
+
     return remark
 
 
 if __name__=="__main__":
 
-    initial_x=[0.5, 0.5]
+    initial_x=[0.5, 0.5 , 0.5]
     t = np.linspace(0, 800, 800)
     punish = 0.5
-    b = 2
+    b = 1
     xi = 0.01
     remark=get_remark(b,punish,xi)
     result = odeint(Cooperation_proportion_derivatives, initial_x, t, args=(punish, b, xi))
