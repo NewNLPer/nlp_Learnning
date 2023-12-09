b83f2df9c2e23892db5947c1f856d998b0d3e633
jiezhoujif <jiezhoujif@126.com>
2023-12-09 16:00:29 +0800

    some code change

--- evolutionary_game/evolution_of_environmental_factors.py
+++ evolutionary_game/evolution_of_environmental_factors.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
 import time
-
+from tqdm import tqdm
 
 
 
@@ -22,15 +22,10 @@ def Cooperation_proportion_derivatives(x, t, punish, b, xi, p_1, p_2):
     :return:
     """
     piC = x[0] - x[1] * punish * (1 - x[0])
-
     piD = (b - x[2] * punish) * x[0]
-
     function_1 = x[0] * ( 1 - x[0] ) * (piC - piD)
-
     function_2 = xi * x[1] * ( 1 - x[1] ) * (p_1 * piD - piC)
-
     function_3 = xi * x[2] * ( 1 - x[2] ) * (p_2 * piC - piD )
-
     return [function_1, function_2, function_3]
 
 def get_plot(x,t,remark):
@@ -39,42 +34,43 @@ def get_plot(x,t,remark):
     C_Perceived_degree=[sublist[1] for sublist in x]
     D_Perceived_degree = [sublist[2] for sublist in x]
 
-    plt.figure(figsize=(12, 12))
+    # plt.figure(figsize=(12, 20))
 
-    plt.subplot(3, 1, 1)
+    # plt.subplot(3, 1, 1)
     plt.plot(t,Collaborator_ratio)
-    plt.xlabel('t')
+    plt.xlabel('b')
     plt.ylabel('c')
     plt.title("Collaborator_ratio")
-
-    plt.subplot(3, 1, 2)
-    plt.plot(t,C_Perceived_degree)
-    plt.xlabel('t')
-    plt.ylabel('p')
-    plt.title("C_Perceived_degree")
-
-    plt.subplot(3, 1, 3)
-    plt.plot(t,D_Perceived_degree)
-    plt.xlabel('t')
-    plt.ylabel('p')
-    plt.title("D_Perceived_degree")
-
-    plt.suptitle("Parameter settings {}".format(remark))
-
-    plt.savefig(r'C:/Users/NewNLPer/Desktop/za/exp_figure/{}.png'.format(remark))
     plt.show()
+
+    # plt.subplot(3, 1, 2)
+    plt.plot(t,C_Perceived_degree)
+    plt.xlabel('b')
+    plt.ylabel('ac')
+    plt.title("C_Perceived_degree")
+    plt.show()
+
+    # plt.subplot(3, 1, 3)
+    plt.plot(t,D_Perceived_degree)
+    plt.xlabel('b')
+    plt.ylabel('ad')
+    plt.title("D_Perceived_degree")
+    plt.show()
+
+    # plt.suptitle("Parameter settings {}".format(remark))
+    # plt.savefig(r'C:/Users/NewNLPer/Desktop/za/exp_figure/{}.png'.format(remark))
+    # plt.show()
+
 
 
 def get_remark(b,punish,xi,p_1,p_2):
-
     remark="b={}_punish={}_xi={}_p1={}_p2={}".format(b,punish,xi,p_1,p_2)
-
     return remark
+
 
 def linespace(start,end,interval):
 
     float_lens=len(str(interval).split(".")[-1])
-    print(float_lens)
     save_list=[]
     while start<=end:
         save_list.append(start)
@@ -84,22 +80,42 @@ def linespace(start,end,interval):
         save_list.append(end)
     return save_list
 
+def get_round(list):
+    for i in range(len(list)):
+        list[i]=round(list[i],3)
+    return list
 
 
 if __name__=="__main__":
 
-    start_time=time.time()
+
     initial_x=[0.5, 0.5 , 0.5]
     t = np.linspace(0, 2000, 2000)
-    punish = 0.5
-    b = 1.003
+    punish = 0.1
     xi = 0.01
     p_1 = 0.5
     p_2 = 1.5
-    remark=get_remark(b,punish,xi,p_1,p_2)
-    result = odeint(Cooperation_proportion_derivatives, initial_x, t, args=(punish, b, xi,p_1,p_2))
-    end_time=time.time()
-    print("time consume : {}".format(end_time-start_time))
-    get_plot(result,t,remark)
+
+
+    result_=[]
+
+    line_space_b=linespace(1,2,0.0001)
+    for b in tqdm(line_space_b):
+        # remark=get_remark(b,punish,xi,p_1,p_2)
+        """
+        round(3)
+        
+        """
+        result = odeint(Cooperation_proportion_derivatives, initial_x, t, args=(punish, b, xi,p_1,p_2))
+        result_.append(get_round(result[-1].tolist()))
+
+    get_plot(result_,line_space_b," ")
+
+
+    # get_plot(result,t,remark)
+
+
+
+
 
 
