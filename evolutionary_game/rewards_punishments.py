3e595342c2d3a4c6a7218bad99d836932549fc52
jiezhoujif <jiezhoujif@126.com>
2023-12-13 12:52:31 +0800

    some code change

--- evolutionary_game/rewards_punishments.py
+++ evolutionary_game/rewards_punishments.py
@@ -5,112 +5,96 @@
 coding with comment！！！
 """
 
-"""
-1.时间演化图
-2.随便变化图
-"""
 
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
-import time
 from tqdm import tqdm
 
 
-
-
-def Cooperation_proportion_derivatives(x, t, punish, b, xi, p_1, p_2):
+def Cooperation_proportion_derivatives(x, t, punish, b, xi):
     """
-    :param x:  Initial variable[x,ac,ad]
+    :param x:  Initial variable[x,pr]
     :param t: time
     :param punish:
     :param b: b=[1,2]
     :param xi:Growth rate control
     :return:
     """
-    piC = x[0] - x[1] * punish * (1 - x[0])
-    piD = (b - x[2] * punish) * x[0]
-    function_1 = x[0] * ( 1 - x[0] ) * (piC - piD)
-    function_2 = xi * x[1] * ( 1 - x[1] ) * (p_1 * piD - piC)
-    function_3 = xi * x[2] * ( 1 - x[2] ) * (p_2 * piC - piD )
-    return [function_1, function_2, function_3]
 
-def get_plot(x,t,remark):
+    piC = x[0] + x[1] * punish * (1 - x[0])
+    piD = (b - x[1] * punish) * x[0]
 
-    Collaborator_ratio= [sublist[0] for sublist in x]
-    C_Perceived_degree=[sublist[1] for sublist in x]
-    D_Perceived_degree = [sublist[2] for sublist in x]
+    function_1 = x[0] * (1 - x[0]) * (piC - piD)
+    function_2 = xi * x[1] * (1 - x[1]) * (piD - piC)
 
-    # plt.figure(figsize=(12, 20))
+    return [function_1, function_2]
 
-    # plt.subplot(3, 1, 1)
+def get_plot(x,t):
+
+    Collaborator_ratio = [sublist[0] for sublist in x]
+    Degree_of_rewards_and_punishments = [sublist[1] for sublist in x]
+
     plt.plot(t,Collaborator_ratio)
     plt.xlabel('b')
-    plt.ylabel('c')
+    plt.ylabel('pc')
     plt.title("Collaborator_ratio")
     plt.show()
 
-    # plt.subplot(3, 1, 2)
-    plt.plot(t,C_Perceived_degree)
+    plt.plot(t,Degree_of_rewards_and_punishments)
     plt.xlabel('b')
-    plt.ylabel('ac')
-    plt.title("C_Perceived_degree")
+    plt.ylabel('degree')
+    plt.title("Degree_of_rewards_and_punishments")
     plt.show()
 
-    # plt.subplot(3, 1, 3)
-    plt.plot(t,D_Perceived_degree)
-    plt.xlabel('b')
-    plt.ylabel('ad')
-    plt.title("D_Perceived_degree")
-    plt.show()
+
 
     # plt.suptitle("Parameter settings {}".format(remark))
     # plt.savefig(r'C:/Users/NewNLPer/Desktop/za/exp_figure/{}.png'.format(remark))
     # plt.show()
 
 
-
-def get_remark(b,punish,xi,p_1,p_2):
-    remark="b={}_punish={}_xi={}_p1={}_p2={}".format(b,punish,xi,p_1,p_2)
+def get_remark(b,punish,xi):
+    remark="b={}_punish={}_xi={}".format(b,punish,xi)
     return remark
+
 
 
 def linespace(start,end,interval):
 
-    float_lens=len(str(interval).split(".")[-1])
-    save_list=[]
-    while start<=end:
+    float_lens = len(str(interval).split(".")[-1])
+    save_list = []
+    while start <= end:
         save_list.append(start)
-        start+=interval
-        start=round(start,float_lens)
+        start += interval
+        start = round(start,float_lens)
     if save_list[-1] != end:
         save_list.append(end)
     return save_list
 
 def get_round(list):
-    for i in range(len(list)):
-        list[i]=round(list[i],3)
-    return list
+    return [round(item,3) for item in list]
+
 
 
 if __name__=="__main__":
 
-    initial_x=[0.5,0.5,0.5]
+    initial_x=[0.5,0]
     t = np.linspace(0,2000,2000)
     punish = 0.1
     xi = 0.01
-    p_1 = 0.5
-    p_2 = 1.5
 
     result_=[]
-
     line_space_b=linespace(1,2,0.0001)
+
+    result = odeint(Cooperation_proportion_derivatives, initial_x, t, args=(punish, xi))
+
     for b in tqdm(line_space_b):
 
-        result = odeint(Cooperation_proportion_derivatives, initial_x, t, args=(punish, b, xi,p_1,p_2))
+        result = odeint(Cooperation_proportion_derivatives, initial_x, t, args=(punish, b, xi))
         result_.append(get_round(result[-1].tolist()))
 
-    get_plot(result_,line_space_b," ")
+    get_plot(result_,line_space_b)
 
 
     # get_plot(result,t,remark)
