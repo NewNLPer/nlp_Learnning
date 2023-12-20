dae62f7b798e98d79a381f1e2ab32c680ae3284f
jiezhoujif <jiezhoujif@126.com>
2023-12-20 21:44:47 +0800

    some code change

--- Flask_learning/flask_learning.py
+++ Flask_learning/flask_learning.py
@@ -4,5 +4,12 @@
 @time: 2023/12/20 21:12
 coding with comment！！！
 """
+from flask import Flask
+flask_learning = Flask(__name__)
 
+@flask_learning.route('/')
+def hello_world():
+    return 'Hello, World!'
 
+if __name__ == "__main__":
+    flask_learning.run()
