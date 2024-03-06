1f3895992d0391e2c98e3e034d82602b5bea108d
jiezhoujif <jiezhoujif@126.com>
2024-03-06 19:25:53 +0800

    some code change

--- LeetCode/2023_3_5_一刷.py
+++ LeetCode/2023_3_5_一刷.py
@@ -4,7 +4,7 @@
 @time: 2024/3/5 12:00
 coding with comment！！！
 """
-## 1.快速排序
+## 快速排序
 def quick_sort(nums,left,right):
     def take_nums(nums,left,right):
         tmp = nums[left]
@@ -15,18 +15,16 @@ def quick_sort(nums,left,right):
             while left < right and nums[left] <= tmp:
                 left += 1
             nums[left],nums[right] = nums[right],nums[left]
-
         nums[left] = tmp
         return left
     if left < right:
         mid = take_nums(nums,left,right)
-        take_nums(nums,left,mid - 1)
-        take_nums(nums,mid + 1,right)
+        quick_sort(nums,left,mid - 1)
+        quick_sort(nums,mid + 1,right)
         return nums
-nums = [1,3,2,2,1,3,4,6]
-# print(quick_sort(nums,0,len(nums) - 1))
 
-## 2.top_k 问题
+## top_k
+
 def top_k(nums,k):
     def sift(nums,low,high):
         tmp = nums[low]
@@ -43,8 +41,8 @@ def top_k(nums,k):
                 break
         nums[i] = tmp
     head = nums[:k]
-    for i in range((k - 2) // 2,-1,-1):
-        sift(head,i,k - 1)
+    for i in range((k-2)//2,-1,-1):
+        sift(head,i,k-1)
     for i in range(k,len(nums)):
         if nums[i] > head[0]:
             head[0] = nums[i]
@@ -53,111 +51,119 @@ def top_k(nums,k):
         head[0],head[i] = head[i],head[0]
         sift(head,0,i-1)
     return head
-# print(top_k(nums,5))
 
-## 3 rand5 去实现rang7
-'''
-首先一定要产生0，然后再相加
-'''
-def lengthOfLongestSubstring(s):
-    if len(s) <= 1:
-        return len(s)
-    else:
-        start_zhen = 0
-        end_zhen = 1
-        res = 1
-        dic = {}
-        dic[s[start_zhen]] = 1
-        dic[s[end_zhen]] = dic.get(s[end_zhen],0) + 1
-        while end_zhen < len(s):
-            if len(dic) == sum(dic.values()):
-                res = max(res,end_zhen - start_zhen + 1)
-                end_zhen += 1
-                if end_zhen < len(s):
-                    dic[s[end_zhen]] = dic.get(s[end_zhen],0) + 1
-            else:
-                pass
-
-## 接雨水 - 画图示意
-
-## 二分查找
- # 左闭右闭
-def erfenchazhao(nums,target): # 如果找不到，start的地方就是需要插的位置
-    start = 0
-    end = len(nums) - 1
-    while start <= end:
-        mid = (start + end) // 2
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] > target:
-            end = mid -1
-        else:
-            start = mid + 1
-    return -1,start
-
-nums = [1,4,5,6,7,9]
-# print(erfenchazhao(nums,6.5))
-### 按照排序，其中存在重复元素，要找到
-
-def get_r(nums,target):
-    start_zhen = 0
-    end_zhen = len(nums)-1
-    while start_zhen <= end_zhen:
-        mid = (start_zhen+end_zhen)//2
-        if nums[mid] > target:
-            end_zhen = mid - 1
-        elif nums[mid] < target:
-            start_zhen = mid + 1
-        else:
-            start_zhen = mid + 1
-    if end_zhen < 0 or nums[end_zhen] != target:
-        return -1
-    return end_zhen
-
-nums = [1,2,3,4,5]
-
-# print(get_r(nums,3))
-
-
-
-def search(nums, target):
-    if len(nums)==1:
-        if target in nums:
-            return 0
-        else:
-            return -1
-    else:
-        start=0
-        end=len(nums)-1
-        while start<=end:
-            mid=(start+end)//2
-            if nums[mid]==target:
-                return mid
-            elif nums[mid]>=nums[start]:#左侧有序
-                if nums[start]<=target<nums[mid]:
-                    end=mid-1
-                else:
-                    start=mid+1
-            else:
-                if nums[mid]<target<=nums[end]:
-                    start=mid+1
-                else:
-                    end=mid-1
-        return -1
-
-
-## 回溯算法
+## 子集
 
 def subset(nums):
     res = []
-    nums.sort()
-    def bt(path,start):
+    def bt(start,path):
         res.append(path[:])
         for i in range(start,len(nums)):
             path.append(nums[i])
-            bt(path,i+1)
+            bt(i+1,path)
             path.pop()
-    bt([],0)
+    bt(0,[])
     return res
-print(subset([1,2,3]))
+## 有重复
+def subset_2(nums):
+    res = []
+    nums.sort()
+    def bt(start,path):
+        res.append(path[:])
+        for i in range(start,len(nums)):
+            if i == start or nums[i] != nums[i-1]:
+                path.append(nums[i])
+                bt(i+1,path)
+                path.pop()
+    bt(0,[])
+    return res
+
+# 排列
+def pailie(nums):
+    n = len(nums)
+    res = []
+    nums.sort()
+    def bt(path,nums):
+        if len(path) == n:
+            res.append(path[:])
+        for i in range(len(nums)):
+            if not i or nums[i] != nums[i-1]:
+                path.append(nums[i])
+                bt(path,nums[:i]+nums[i+1:])
+                path.pop()
+    bt([],nums)
+    return res
+
+
+## 回文切割
+def select_s(s):
+    res = []
+    def bt(start,path):
+        if start >= len(s):
+            res.append(path[:])
+        for i in range(start,len(s)):
+            p = s[start:i + 1]
+            if p == p[: : - 1]:
+                path.append(p)
+                bt(i+1,path)
+                path.pop()
+            else:
+                continue
+    bt(0,[])
+    return res
+
+
+def erfenchazhao(nums,taregt):
+    start_zhen = 0
+    end_zhen  = len(nums) - 1
+    while start_zhen <= end_zhen:
+        mid = (start_zhen + end_zhen) // 2
+        if nums[mid] == taregt:
+            return mid
+        elif nums[mid] > taregt:
+            end_zhen = mid - 1
+        else:
+            start_zhen = mid + 1
+    return -1
+
+def get_r(nums,target):
+    start_zhen = 0
+    end_zhen  = len(nums) - 1
+    while start_zhen <= end_zhen:
+        mid = (start_zhen + end_zhen) // 2
+        if nums[mid] > target:
+            end_zhen = mid - 1
+        elif nums[mid] < mid:
+            start_zhen = mid + 1
+        else:
+            start_zhen = mid + 1
+    if end_zhen < 0 and nums[end_zhen] != target:
+        return -1
+    return end_zhen
+
+
+
+
+
+
+dic = {"asda":1,"asdaa":2}
+print(dic)
+dic["ad"] = 2
+print(dic)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
