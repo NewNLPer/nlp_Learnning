8d64a112dedf7deec3905250006b67c1554a3037
jiezhoujif <jiezhoujif@126.com>
2024-05-29 12:55:10 +0800

    some code change

--- langchain_learning/goc.sh
+++ langchain_learning/goc.sh
@@ -5,11 +5,11 @@ cd C:\Users\NewNLPer\Desktop\za\nlp_learning\nlp_learnning\langchain_learning
 
 python langchain_project.py \
     --baichuan_api_keys "eeb680feddd64f2867b5f156a4f1d3e2.Gub5EM1kBosl6894" \
-    --knowledge_file_path_list ["C:\Users\NewNLPer\Desktop\基于环境影响的合作演化模型.pdf"] \
-    --em_model_path "D:\google下载" \
-    --fassi_save_path "C:\Users\NewNLPer\Desktop\test\fassi_save" \
-    --url_setting "127.0.0.1" \
-    --pdf_combine_path "C:\Users\NewNLPer\Desktop\test\combine_file.pdf" \
+    --knowledge_file_path_list ["/home/ubuntu/jijiezhou/data_save/pdf_data/compute_net.pdf"] \
+    --em_model_path "/home/ubuntu/em_model" \
+    --fassi_save_path "/home/ubuntu/jijiezhou/data_save/fassi_save_path" \
+    --url_setting "192.168.32.23" \
+    --pdf_combine_path "/home/ubuntu/jijiezhou/data_save/pdf_data/combine_file.pdf" \
     --n_gram 3
 
 
